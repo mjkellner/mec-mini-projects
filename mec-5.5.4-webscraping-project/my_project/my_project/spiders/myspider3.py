86bebe4f204542fb2775d84a0b0af13059818593
mjkellner <32286495+mjkellner@users.noreply.github.com>
2023-02-01 13:03:44 -0700

    Create myspider3.py

---
+++ mec-5.5.4-webscraping-project/my_project/my_project/spiders/myspider3.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 11:29:01 2023
+
+@author: t820608
+"""
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "myspider3"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
