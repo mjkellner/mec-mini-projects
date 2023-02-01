06542c6bd3ba87499f00a61b1637dc541dd18654
mjkellner <32286495+mjkellner@users.noreply.github.com>
2023-02-01 13:03:52 -0700

    Create myspider6.py

---
+++ mec-5.5.4-webscraping-project/my_project/my_project/spiders/myspider6.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 11:38:35 2023
+
+@author: t820608
+"""
+
+import scrapy
+
+
+class AuthorSpider(scrapy.Spider):
+    name = 'myspider6'
+
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        author_page_links = response.css('.author + a')
+        yield from response.follow_all(author_page_links, self.parse_author)
+
+        pagination_links = response.css('li.next a')
+        yield from response.follow_all(pagination_links, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
