55917e767cf16c702f9cafbee54bba86e4898ca5
mjkellner <32286495+mjkellner@users.noreply.github.com>
2023-02-01 13:03:30 -0700

    Create myspider1.py

---
+++ mec-5.5.4-webscraping-project/my_project/my_project/spiders/myspider1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "myspider1"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
