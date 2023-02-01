25cba9487864e1e6464296181de3019ab8b88337
mjkellner <32286495+mjkellner@users.noreply.github.com>
2023-02-01 13:03:40 -0700

    Create myspider2.py

---
+++ mec-5.5.4-webscraping-project/my_project/my_project/spiders/myspider2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "myspider2"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
